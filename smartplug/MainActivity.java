f3afca269f7b99d3411501b5344189d54604b9c8
Yusuf <34628247+yusufkrcm@users.noreply.github.com>
2019-03-18 19:53:22 +0300

    Add files via upload

---
+++ smartplug/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.yusuf.smartplug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText UsernameEt, PasswordEt;
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        UsernameEt = (EditText) findViewById(R.id.etUserName);
+        PasswordEt = (EditText)  findViewById(R.id.etPassword);
+    }
+
+    public void OnLogin(View view){
+        String username = UsernameEt.getText().toString();
+        String password = PasswordEt.getText().toString();
+        String type = "login";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, username, password);
+    }
+}
